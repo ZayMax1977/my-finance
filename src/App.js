1eb2c64c28fe01f88f02e1d0bc1a6cb6d98d14bd
Max Zanchenko <zamax77@mail.ru>
2023-02-04 16:34:10 +0300

    2.14-props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import CostItem from './components/CostItem'
 function App() {
+
+  const costs =[
+    {
+      date:new Date(2021,2,12),
+      describtion: 'Холодильник',
+      amount: 999.99
+    },
+    {
+      date:new Date(2021,5,4),
+      describtion: 'ПК',
+      amount: 1254.57
+    },
+    {
+      date:new Date(2021,9,30),
+      describtion: 'Джинсы',
+      amount: 123.23
+    }
+
+  ]
   return (
     <div>
       <h1>Начнем изучение React</h1>
-      <CostItem></CostItem>
+      <CostItem 
+        date ={costs[0].date}
+        describtion={costs[0].describtion}
+        amount = {costs[0].amount}>
+      </CostItem>
+
+      <CostItem
+        date ={costs[1].date}
+        describtion={costs[1].describtion}
+        amount = {costs[1].amount}>
+      </CostItem>
+
+      <CostItem
+        date ={costs[2].date}
+        describtion={costs[2].describtion}
+        amount = {costs[2].amount}>
+      </CostItem>
+
 
     </div>
    
