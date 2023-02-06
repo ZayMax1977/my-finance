6fca7d29b4fa36b33fd2829d3160bf5ba1a574d2
Max Zanchenko <zamax77@mail.ru>
2023-02-06 10:40:44 +0300

    3 значения из формы через состояние, и в консоль

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,8 +1,23 @@
+import  React, {useState} from "react";
 import './CostForm.css'
 
 const CostForm = () =>{
+    const [name,setName] = useState("");
+    const [amount,setAmount] = useState("");
+    const [date,setDate] = useState("");
+
+
     const NameChangeHandler =(e) =>{
-        console.log(e.target.value);
+        setName(e.target.value);
+        console.log(name);
+    };
+    const AmountChangeHandler =(e) =>{
+        setAmount(e.target.value);
+        console.log(amount);
+    };
+    const DateChangeHandler =(e) =>{
+        setDate(e.target.value);
+        console.log(date);
     };
     return (                               
         <form>
@@ -13,11 +28,11 @@ const CostForm = () =>{
                 </div>
                 <div className='new-cost__control label'>
                     <label>Сумма</label>
-                    <input type='number' min='0.01' step='0.01'/>
+                    <input type='number' min='0.01' step='0.01' onChange={AmountChangeHandler}/>
                 </div>
                 <div className='new-cost__control label'>
                     <label>Дата</label>
-                    <input type='date' min='2023-02-06' step='2025-02-06'/>
+                    <input type='date' min='2023-02-06' step='2025-02-06' onChange={DateChangeHandler}/>
                 </div>
                 <div className='new-cost__actions'>
                     <button type='submit'>Добавить Расход</button>
