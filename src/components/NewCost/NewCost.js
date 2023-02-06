c873e2930a959029acf1957e91eb99be627b334f
Max Zanchenko <zamax77@mail.ru>
2023-02-06 16:05:41 +0300

    данные снизу вверх на один уровень

--- src/components/NewCost/NewCost.js
+++ src/components/NewCost/NewCost.js
@@ -2,12 +2,19 @@ import './NewCost.css'
 import CostForm from './CostForm'
 
 const NewCost = () =>{
+    const saveCostDataHandler = (inputCostData)=>{
+        const costData = {
+            ...inputCostData,
+            id:Math.random().toString(),
+        };
+        console.log(costData);
+    };
     return (
         <div className='new-cost'>            
-<CostForm/>
+<CostForm onSaveCostData={saveCostDataHandler}/>
         </div>
         
     );
-}
+};
 
 export default NewCost;
