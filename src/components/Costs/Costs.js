72539395437302822b81a2e062a1a5b6974c926a
Max Zanchenko <zamax77@mail.ru>
2023-02-07 14:21:29 +0300

    делаем вывод расходов динамическим - map

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -9,31 +9,18 @@ function Costs(props){
 
     const yearChangeHandler = (year)=>{
       setSelectedYear(year)
-      console.log(year + "costs")
     }
-
+    
     return(
-        
-        
         <Card className='costs'>
         <CostsFilter year={selectedYear} onChangeYear ={yearChangeHandler}/>
-        <CostItem 
-            date ={props.costs[0].date}
-            description={props.costs[0].description}
-            amount = {props.costs[0].amount}/>
-
-      <CostItem
-        date ={props.costs[1].date}
-        description={props.costs[1].description}
-        amount = {props.costs[1].amount}/>
-      
-
-      <CostItem
-        date ={props.costs[2].date}
-        description={props.costs[2].description}
-        amount = {props.costs[2].amount}/>
+        {props.costs.map((cost) => (
+          <CostItem 
+          date ={cost.date}
+          description={cost.description}
+          amount = {cost.amount}/>
+        ))}
       </Card>
-      
     );
 }
 
