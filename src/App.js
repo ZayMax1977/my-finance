7379db28111737583a5ba0e682a34e3a2a06b515
Max Zanchenko <zamax77@mail.ru>
2023-02-08 11:49:53 +0300

    обновляем предыдущий список покупок

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import Costs from './components/Costs/Costs'
 import NewCost from './components/NewCost/NewCost';
+import React,{useState} from 'react';
+
+
+const INITIAL_COSTS =[
+  {
+    id: 'c1',
+    date:new Date(2021,2,12),
+    description: 'Холодильник',
+    amount: 999.99
+  },
+  {
+    id: 'c2',
+    date:new Date(2021,5,4),
+    description: 'ПК',
+    amount: 1254.57
+  },
+  {
+    id: 'c3',
+    date:new Date(2021,9,30),
+    description: 'Джинсы',
+    amount: 123.23
+  }
+
+]
 
 function App() {
-
-  const costs =[
-    {
-      id: 'c1',
-      date:new Date(2021,2,12),
-      description: 'Холодильник',
-      amount: 999.99
-    },
-    {
-      id: 'c2',
-      date:new Date(2021,5,4),
-      description: 'ПК',
-      amount: 1254.57
-    },
-    {
-      id: 'c3',
-      date:new Date(2021,9,30),
-      description: 'Джинсы',
-      amount: 123.23
-    }
-
-  ]
+  const [costs,setCosts] =  useState(INITIAL_COSTS)
+  
   const addCostHandler = (cost) =>{
-    console.log(cost);
-    console.log('App component')
-  } 
+    setCosts((prevCosts)=>{
+    return [cost,...prevCosts];
+  });
+  } ;
   return (
     <div>
         <NewCost onAddCost={addCostHandler}/>      
@@ -39,4 +44,3 @@ function App() {
 export default App;
 
 
-//3.13
